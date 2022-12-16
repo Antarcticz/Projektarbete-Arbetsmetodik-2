228937b02d900be60b6cf72b046e206754103b3f
Felix Edström <felix.edstrom1@gmail.com>
2022-12-16 19:59:07 +0100

    section-4 klar

--- app.js
+++ app.js
@@ -53,3 +53,29 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
 
 }
+
+document.addEventListener('click', e => {
+  let handle
+  if (e.target.matches(".handle")) {
+      handle = e.target
+  } else {
+      handle = e.target.closest(".handle")
+  }
+  if (handle != null) onHandleClick(handle)
+});
+
+function onHandleClick(handle) {
+  let leftHandle = document.querySelector('left-handle');
+  const slider = handle.closest('.carousel-4-container').querySelector('.slider');
+  const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue('--slider-index'))
+  if (handle.classList.contains('left-handle')) {
+      slider.style.setProperty('--slider-index', sliderIndex - 1)
+  }
+  if (handle.classList.contains('right-handle')) {
+      slider.style.setProperty('--slider-index', sliderIndex + 1)
+  }
+  
+console.log(slider)
+}
+
+onHandleClick();
