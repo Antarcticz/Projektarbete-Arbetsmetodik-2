bc22843c59c67784cc23c51ddac9f0b67b4f3881
Maximilian <maximilian_hedman@hotmail.com>
2022-12-20 23:36:19 +0100

    fixat koden i section 3 och 1

--- app.js
+++ app.js
@@ -57,6 +57,37 @@ showSlides = (n) => {
 
 
 
+var swiper = new Swiper(".karusell-1", {
+  slidesPerView: 10,
+  spaceBetween: 1,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".karusell-3", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 var swiper = new Swiper(".felix", {
   slidesPerView: 5,
@@ -82,20 +113,5 @@ var swiper = new Swiper(".karusell-7", {
   },
 });
 
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  slidesPerGroup: 3,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev"
-  }
-});
 
 
